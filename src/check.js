659c4097db4970c46e127f5093bd2d39b03f0790
Embry-Bit <64559194+Embry-Bit@users.noreply.github.com>
2021-07-05 16:37:28 -0400

    Update check.js

--- src/check.js
+++ src/check.js
@@ -3,25 +3,10 @@ const permute = require('email-permutator');
 const fetch = require('node-fetch')
 
 
-// const validate = async (email) => {
-
-// axios.get('https://emailvalidation.abstractapi.com/v1/?api_key=85cc83580ab34412ab85f31752670f99&email='+email)
-//     .then(response => {
-//         // console.log(response.data);
-//         return response.data
-//     })
-//     .catch(error => {
-//         console.log(error);
-//     });
-
-// }
-
-
-
 
 const validate = async (email) => {
 
-    return await fetch('https://emailvalidation.abstractapi.com/v1/?api_key=85cc83580ab34412ab85f31752670f99&email='+email, {
+    return await fetch('https://emailvalidation.abstractapi.com/v1/?api_key='+process.env.TOKEN+'&email='+email, {
     })
     .then((res) => { 
         status = res.status; 
